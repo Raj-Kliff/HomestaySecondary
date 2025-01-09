07a418f193f598f06960e6be97bbf1836391eb00
Raj <raj@gmail.com>
2025-01-09 16:37:26 +0530

    signup page changed to inital

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 import facebookSvg from '@/images/Facebook.svg'
 import twitterSvg from '@/images/Twitter.svg'
 import googleSvg from '@/images/Google.svg'
@@ -7,31 +6,20 @@ import Input from '@/shared/Input'
 import ButtonPrimary from '@/shared/ButtonPrimary'
 import Image from 'next/image'
 import Link from 'next/link'
-import Select from '@/shared/Select'
-import axios from 'axios'
-import ReCAPTCHA from "react-google-recaptcha";
-
-const countryCodes = require('country-codes-list')
-
-// Define a type for the country code options
-interface CountryCode {
-	code: string;
-	callingCode: string;
-}
 
 export interface PageSignUpProps {}
 
-const registerSocials = [
+const loginSocials = [
 	{
 		name: 'Continue with Facebook',
 		href: '#',
 		icon: facebookSvg,
 	},
-	// {
-	// 	name: 'Continue with Twitter',
-	// 	href: '#',
-	// 	icon: twitterSvg,
-	// },
+	{
+		name: 'Continue with Twitter',
+		href: '#',
+		icon: twitterSvg,
+	},
 	{
 		name: 'Continue with Google',
 		href: '#',
@@ -40,68 +28,15 @@ const registerSocials = [
 ]
 
 const PageSignUp: FC<PageSignUpProps> = ({}) => {
-
-	const [countryCodesList, setCountryCodesList] = useState<CountryCode[]>([]);
-	const [configData, setConfigData] = useState();
-	const [registerSocial, setRegisterSocial] = useState();
-
-	const { NEXT_PUBLIC_API_BASE_URL } = process.env;
-	console.log('process:',NEXT_PUBLIC_API_BASE_URL)
-	
-	const fetchConfigData = async() => {
-		console.log('process1:',NEXT_PUBLIC_API_BASE_URL)
-		try { 
-			const {data} = await axios.get('https://homestay.kliffhost.in/api/register-config',{
-				headers: {
-					"x-api-key": '8a6dfc93-e244-40e5-9474-f86a5d3696ff'
-				},
-			})
-			if(data.status == 'success'){
-				setConfigData(data)
-			}
-			
-		} catch (error) {
-			console.log(error)
-		}
-	}
-
-	function onChange(value:any) {
-		console.log("Captcha value:", value);
-	  }
-
-	useEffect(()=>{
-		fetchConfigData()
-	},[])
-
-
-	// country code 
-    useEffect(() => {
-        // Get the custom list of country codes
-        const countryCodesObject = countryCodes.customList('countryCode', '[{countryCode}] {countryNameEn}: +{countryCallingCode}');
-        
-        // Extract only the country code and calling code for display
-        const countryCodesArray = Object.keys(countryCodesObject).map((key) => {
-            const countryData = countryCodesObject[key];
-            const match = countryData.match(/\+(\d+)/); // Extract the calling code
-            return {
-                code: key,
-                callingCode: match ? match[0] : '',  // Default to empty if no match
-            };
-        });
-
-        setCountryCodesList(countryCodesArray); // Set the state with country codes and calling codes
-    }, []);
-
-
 	return (
 		<div className={`nc-PageSignUp`}>
 			<div className="container mb-24 lg:mb-32">
-				<h2 className="my-10 mt-5 flex items-center justify-center text-3xl font-semibold leading-[115%] text-neutral-900 dark:text-neutral-100 md:text-5xl md:leading-[115%]">
+				<h2 className="my-20 flex items-center justify-center text-3xl font-semibold leading-[115%] text-neutral-900 dark:text-neutral-100 md:text-5xl md:leading-[115%]">
 					Signup
 				</h2>
 				<div className="mx-auto max-w-md space-y-6">
 					<div className="grid gap-3">
-						{/* {registerSocials.map((item, index) => (
+						{loginSocials.map((item, index) => (
 							<a
 								key={index}
 								href={item.href}
@@ -116,42 +51,7 @@ const PageSignUp: FC<PageSignUpProps> = ({}) => {
 									{item.name}
 								</h3>
 							</a>
-						))} */}
-
-						{
-							configData?.data?.social.facebook_login === '1' && (
-								<a
-									href='#'
-								className="nc-will-change-transform flex w-full transform rounded-lg bg-primary-50 px-4 py-3 transition-transform hover:translate-y-[-2px] dark:bg-neutral-800 sm:px-6"
-							>
-								<Image
-									className="flex-shrink-0"
-									src={facebookSvg}
-									alt={'facebook'}
-								/>
-								<h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-									{'Continue with Facebook'}
-								</h3>
-							</a>
-							)
-						}
-						{
-							configData?.data?.social.facebook_login === '1' && (
-								<a
-									href='#'
-								className="nc-will-change-transform flex w-full transform rounded-lg bg-primary-50 px-4 py-3 transition-transform hover:translate-y-[-2px] dark:bg-neutral-800 sm:px-6"
-							>
-								<Image
-									className="flex-shrink-0"
-									src={googleSvg}
-									alt={'google'}
-								/>
-								<h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-									{'Continue with Google'}
-								</h3>
-							</a>
-							)
-						}
+						))}
 					</div>
 					{/* OR */}
 					<div className="relative text-center">
@@ -162,59 +62,15 @@ const PageSignUp: FC<PageSignUpProps> = ({}) => {
 					</div>
 					{/* FORM */}
 					<form className="grid grid-cols-1 gap-6" action="#" method="post">
-						<div className='flex gap-3'>
-							<label className="block">
-								<span className="text-neutral-800 dark:text-neutral-200">
-									First Name
-								</span>
-								<Input
-									type="text"
-									placeholder="First Name"
-									className="mt-1"
-								/>
-							</label>
-							<label className="block">
-								<span className="text-neutral-800 dark:text-neutral-200">
-									Last Name
-								</span>
-								<Input
-									type="text"
-									placeholder="Last Name"
-									className="mt-1"
-								/>
-							</label>
-						</div>
 						<label className="block">
 							<span className="text-neutral-800 dark:text-neutral-200">
-								Email
+								Email address
 							</span>
 							<Input
 								type="email"
-								placeholder="Email"
+								placeholder="example@example.com"
 								className="mt-1"
 							/>
-						</label>
-						<label className="block">
-							<span className="text-neutral-800 dark:text-neutral-200">
-								Phone
-							</span>
-							<div className='flex items-center gap-2'>
-								<div className='w-[6.5rem]'>
-									<Select>
-										<option value="+91">+91 IN</option>
-										{countryCodesList.map((country) => (
-											<option key={country.code} value={country.callingCode}>
-												{country.callingCode} {country.code}
-											</option>
-										))}
-									</Select>
-								</div>
-								<Input
-									type="number"
-									placeholder="Phone"
-									className="mt-1 w-full flex-1"
-								/>
-							</div>
 						</label>
 						<label className="block">
 							<span className="flex items-center justify-between text-neutral-800 dark:text-neutral-200">
@@ -222,22 +78,6 @@ const PageSignUp: FC<PageSignUpProps> = ({}) => {
 							</span>
 							<Input type="password" className="mt-1" />
 						</label>
-						<label className="block">
-							<span className="flex items-center justify-between text-neutral-800 dark:text-neutral-200">
-								Birthday
-							</span>
-							<Input type="date" className="mt-1" />
-						</label>
-						{
-							configData?.data?.reCaptchaEnable && (
-								<label className='block'>
-								<ReCAPTCHA
-								sitekey="Your client site key"
-								onChange={onChange}
-								/>
-							</label>
-							)
-						}
 						<ButtonPrimary type="submit">Continue</ButtonPrimary>
 					</form>
 
